d43efe14c735170a424e33e0a0b65ab8c7be8fcc
Ricky Garcia <rickyg08@msn.com>
2020-11-11 15:30:27 -0700

    Changed to NavigationView

--- app/src/main/java/edu/cnm/deepdive/codebreaker/controller/GameFragment.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/GameFragment.java
@@ -3,9 +3,6 @@ package edu.cnm.deepdive.codebreaker.controller;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Spinner;
@@ -15,8 +12,6 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import edu.cnm.deepdive.codebreaker.R;
 import edu.cnm.deepdive.codebreaker.adapter.CodeCharacterAdapter;
 import edu.cnm.deepdive.codebreaker.adapter.GuessAdapter;
@@ -38,13 +33,6 @@ public class GameFragment extends Fragment {
   private int codeLength;
   private FragmentGameBinding binding;
   private Spinner[] spinners;
-  private NavController navController;
-
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setHasOptionsMenu(true);
-  }
 
   @Nullable
   @Override
@@ -59,30 +47,7 @@ public class GameFragment extends Fragment {
   @Override
   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
-    setupNavigation(view);
     setupViewModel();
-  }
-
-  @Override
-  public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
-    super.onCreateOptionsMenu(menu, inflater);
-    inflater.inflate(R.menu.game_options, menu);
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-    boolean handled = true;
-    switch (item.getItemId()) {
-      case R.id.new_game:
-        startGame();
-        break;
-      case R.id.settings:
-        navController.navigate(R.id.action_navigation_game_to_navigation_settings);
-        break;
-      default:
-        handled = super.onOptionsItemSelected(item);
-    }
-    return handled;
   }
 
   private void setupMaps() {
@@ -99,8 +64,6 @@ public class GameFragment extends Fragment {
 
   private void setupViews() {
     binding.submit.setOnClickListener((view) -> recordGuess());
-    binding.summary.setOnClickListener((view) ->
-        navController.navigate(R.id.action_navigation_game_to_navigation_summary));
     int maxCodeLength = getResources().getInteger(R.integer.code_length_pref_max);
     spinners = new Spinner[maxCodeLength];
     LayoutInflater inflater = LayoutInflater.from(getContext());
@@ -113,10 +76,6 @@ public class GameFragment extends Fragment {
       spinners[i] = spinner;
       binding.spinners.addView(spinner);
     }
-  }
-
-  private void setupNavigation(View root) {
-    navController = Navigation.findNavController(root);
   }
 
   private void setupViewModel() {
---
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/NavigationActivity.java
@@ -0,0 +1,101 @@
+package edu.cnm.deepdive.codebreaker.controller;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Menu;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration.Builder;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import edu.cnm.deepdive.codebreaker.R;
+import edu.cnm.deepdive.codebreaker.databinding.ActivityNavigationBinding;
+import edu.cnm.deepdive.codebreaker.service.GoogleSignInService;
+import edu.cnm.deepdive.codebreaker.viewmodel.MainViewModel;
+
+public class NavigationActivity extends AppCompatActivity {
+
+  private ActivityNavigationBinding binding;
+  private AppBarConfiguration appBarConfig;
+  private NavController navController;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    binding = ActivityNavigationBinding.inflate(getLayoutInflater());
+    setContentView(binding.getRoot());
+    setSupportActionBar(binding.appBarLayout.toolbar);
+    setupNavigation();
+    setupViewModel();
+  }
+
+  private void setupNavigation() {
+    appBarConfig = new Builder(
+        R.id.navigation_game, R.id.navigation_summary, R.id.navigation_settings)
+        .setOpenableLayout(binding.drawerLayout)
+        .build();
+    //noinspection ConstantConditions
+    navController = ((NavHostFragment) getSupportFragmentManager()
+        .findFragmentById(R.id.nav_host_fragment)).getNavController();
+    NavigationUI.setupActionBarWithNavController(this, navController, appBarConfig);
+    NavigationUI.setupWithNavController(binding.navView, navController);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.navigation, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    boolean handled = true;
+    //noinspection SwitchStatementWithTooFewBranches
+    switch (item.getItemId()) {
+      case R.id.sign_out:
+        logout();
+        break;
+      default:
+        handled = super.onOptionsItemSelected(item);
+    }
+    return handled;
+  }
+
+  @Override
+  public boolean onSupportNavigateUp() {
+    return NavigationUI.navigateUp(navController, appBarConfig)
+        || super.onSupportNavigateUp();
+  }
+
+  private void setupViewModel() {
+    MainViewModel viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+    viewModel.getThrowable().observe(this, (throwable) -> {
+      if (throwable != null) {
+        Toast.makeText(this, throwable.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+      }
+    });
+  }
+
+  private void logout() {
+    GoogleSignInService.getInstance().signOut()
+        .addOnCompleteListener((ingored) -> {
+          Intent intent = new Intent(this, LoginActivity.class)
+              .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+          startActivity(intent);
+        });
+  }
+
+}
